857cec30ef1d3acfe35019d0c90ae81622870e45
Shirokova-Irina <73382441+Shirokova-Irina@users.noreply.github.com>
2020-12-20 18:35:20 +0300

    Delete 8_6.sql

--- бд/8_6.sql
+++
@@ -1,8 +0,0 @@
---Выбрать название триггера, имя таблицы, для которой определен триггер -для всех триггеров, созданных назначенным пользователем базы данных
-
-SELECT  TR.[name] AS [Trigger_Name]
-		,TABL.[name] AS [Table_Name]
-FROM sys.objects TABL 
-JOIN sys.objects TR ON TR.parent_object_id = TABL.[object_id] 
-JOIN sys.schemas sch ON sch.[schema_id] = TR.[schema_id]
-WHERE TR.[type] = 'TR' 
